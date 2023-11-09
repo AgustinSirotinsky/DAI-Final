b992688d4701687ef50a9294b09e2d84194808da
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-09 11:01:54 -0300

    lolsas

---
+++ src/routes/Home.jsx
@@ -0,0 +1,17 @@
+//React
+import { Link } from 'react-router-dom';
+
+//Bootstrap
+import { Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+
+export default function Home() {
+    return (
+        <Container>
+        <div style={{ textAlign: 'center' }}>
+            <h1>Hello World!</h1>
+           
+        </div>
+        </Container>
+    );
+}
